d48b30d4a35182a82e303b48d4d85d00755f389e
Poss111 <dpatposs@msn.com>
2018-06-10 22:30:08 -0500

    Adding google tutorial file - Daniel

---
+++ DisclosureProcessor.py
@@ -0,0 +1,127 @@
+import math
+
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+california_housing_dataframe = pd.read_csv("https://storage.googleapis.com/mledu-datasets/california_housing_train.csv", sep=",")
+
+# california_housing_dataframe = california_housing_dataframe.reindex(
+#     np.random.permutation(california_housing_dataframe.index))
+
+def preprocess_features(california_housing_dataframe):
+    """Prepares input features from California housing data set.
+
+    Args:
+      california_housing_dataframe: A Pandas DataFrame expected to contain data
+        from the California housing data set.
+    Returns:
+      A DataFrame that contains the features to be used for the model, including
+      synthetic features.
+    """
+    selected_features = california_housing_dataframe[
+        ["latitude",
+         "longitude",
+         "housing_median_age",
+         "total_rooms",
+         "total_bedrooms",
+         "population",
+         "households",
+         "median_income"]]
+    processed_features = selected_features.copy()
+    # Create a synthetic feature.
+    processed_features["rooms_per_person"] = (
+            california_housing_dataframe["total_rooms"] /
+            california_housing_dataframe["population"])
+    return processed_features
+
+def preprocess_targets(california_housing_dataframe):
+    """Prepares target features (i.e., labels) from California housing data set.
+
+    Args:
+      california_housing_dataframe: A Pandas DataFrame expected to contain data
+        from the California housing data set.
+    Returns:
+      A DataFrame that contains the target feature.
+    """
+    output_targets = pd.DataFrame()
+    # Scale the target to be in units of thousands of dollars.
+    output_targets["median_house_value"] = (
+            california_housing_dataframe["median_house_value"] / 1000.0)
+    return output_targets
+
+training_examples = preprocess_features(california_housing_dataframe.head(12000))
+training_examples.describe()
+
+training_targets = preprocess_targets(california_housing_dataframe.head(12000))
+training_targets.describe()
+
+validation_examples = preprocess_features(california_housing_dataframe.tail(5000))
+validation_examples.describe()
+
+validation_targets = preprocess_targets(california_housing_dataframe.tail(5000))
+validation_targets.describe()
+
+plt.figure(figsize=(13, 8))
+
+ax = plt.subplot(1, 2, 1)
+ax.set_title("Validation Data")
+
+ax.set_autoscaley_on(False)
+ax.set_ylim([32, 43])
+ax.set_autoscalex_on(False)
+ax.set_xlim([-126, -112])
+plt.scatter(validation_examples["longitude"],
+            validation_examples["latitude"],
+            cmap="coolwarm",
+            c=validation_targets["median_house_value"] / validation_targets["median_house_value"].max())
+
+ax = plt.subplot(1,2,2)
+ax.set_title("Training Data")
+
+ax.set_autoscaley_on(False)
+ax.set_ylim([32, 43])
+ax.set_autoscalex_on(False)
+ax.set_xlim([-126, -112])
+plt.scatter(training_examples["longitude"],
+            training_examples["latitude"],
+            cmap="coolwarm",
+            c=training_targets["median_house_value"] / training_targets["median_house_value"].max())
+_ = plt.plot()
+
+def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
+    """Trains a linear regression model of multiple features.
+
+    Args:
+      features: pandas DataFrame of features
+      targets: pandas DataFrame of targets
+      batch_size: Size of batches to be passed to the model
+      shuffle: True or False. Whether to shuffle the data.
+      num_epochs: Number of epochs for which data should be repeated. None = repeat indefinitely
+    Returns:
+      Tuple of (features, labels) for next data batch
+    """
+
+    # Convert pandas data into a dict of np arrays.
+    features = {key:np.array(value) for key,value in dict(features).items()}
+
+    # Construct a dataset, and configure batching/repeating.
+    ds = Dataset.from_tensor_slices((features,targets)) # warning: 2GB limit
+    ds = ds.batch(batch_size).repeat(num_epochs)
+
+    # Shuffle the data, if specified.
+    if shuffle:
+        ds = ds.shuffle(10000)
+
+    # Return the next batch of data.
+    features, labels = ds.make_one_shot_iterator().get_next()
+    return features, labels
