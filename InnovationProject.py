ed182467ead042b91f21a72650f049eaf7a4e0e4
Poss111 <dpatposs@msn.com>
2018-06-17 22:15:17 -0500

    Updated naming of xaxis and yaxis in graphs - Daniel

--- InnovationProject.py
+++ InnovationProject.py
@@ -29,17 +29,19 @@ feature = preprocessData(sampleLoanData).groupby('loan_id')
 
 def buildGraph(key):
     plt.figure(figsize=(13, 8))
-    ax = plt.subplot(1, 2, 1)
-    ax.set_title(("Loan : ", str(key)))
+    ax = plt.subplot(1, 1, 1)
+    ax.set_title("Loan ID " + str(key))
+    plt.xlabel("Activity Period")
+    plt.ylabel("Unpaid Principle Balance")
     plt.plot(feature.get_group(key)["activity_period"], feature.get_group(key)["unpaid_principal_balance"])
     filepath : str = 'graphs/' + str(key) + '/'
     filename : str = str(key) + 'DataGraph.png'
-    folderPath = os.path.join(directory, filepath)
+    folder_path = os.path.join(directory, filepath)
 
-    if not os.path.isdir(folderPath):
-        os.makedirs(folderPath)
+    if not os.path.isdir(folder_path):
+        os.makedirs(folder_path)
 
-    plt.savefig(folderPath + filename)
+    plt.savefig(folder_path + filename)
 
 def buildGraphs(feature):
     for key, item in feature:
